490a6ebf999e0ece8f07cb2d671c0caaceffdae0
Tristan Hitt <39978624+thitt7@users.noreply.github.com>
2024-02-14 19:50:33 -0600

    cleaned up mapping profile

--- src/url/RequestHelpers/MappingProfiles.cs
+++ src/url/RequestHelpers/MappingProfiles.cs
@@ -7,37 +7,16 @@ namespace URLService.RequestHelpers;
 public class MappingProfiles: Profile
 {
     public MappingProfiles() {
-        // CreateMap<URL, UrlDto>().IncludeMembers(x => x.VisitLogs);
-        // CreateMap<VisitLog, VisitLogDto>();
-        // CreateMap<VisitLog, UrlDto>();
-        // CreateMap<CreateUrlDto, URL>()
-        //     .ForMember(d => d.VisitLogs, o => o.MapFrom(s => s));
-        // CreateMap<CreateUrlDto, VisitLog>();
-
-        // CreateMap<UrlDto, AuctionCreated>();
-        // CreateMap<URL, AuctionUpdated>().IncludeMembers(a => a.VisitLogs);
-        // CreateMap<VisitLog, AuctionUpdated>();
-
-        // CreateMap<URL, UrlDto>().IncludeMembers(x => x.VisitLogs);
+        
         CreateMap<URL, UrlDto>()
             .ForMember(dest => dest.VisitLogs, opt => opt.MapFrom(src => src.VisitLogs));
+
         CreateMap<VisitLog, VisitLogDto>();
-        CreateMap<CreateUrlDto, URL>(); // Mapping from CreateUrlDto to URL entity
 
-        // Mapping from CreateUrlDto to VisitLog entity
+        CreateMap<CreateUrlDto, URL>();
         CreateMap<CreateUrlDto, VisitLogDto>();
-            // .ForMember(d => d.CreatedAt, opt => opt.MapFrom(s => DateTime.UtcNow))
-            // .ForMember(d => d.UserAgent, opt => opt.MapFrom(s => "defaultUserAgent"))
-            // .ForMember(d => d.IPAddress, opt => opt.MapFrom(s => "defaultIPAddress"))
-            // .ForMember(d => d.Continent, opt => opt.MapFrom(s => "defaultContinent"))
-            // .ForMember(d => d.Country, opt => opt.MapFrom(s => "defaultCountry"))
-            // .ForMember(d => d.Region, opt => opt.MapFrom(s => "defaultRegion"))
-            // .ForMember(d => d.City, opt => opt.MapFrom(s => "defaultCity"))
-            // .ForMember(d => d.ISP, opt => opt.MapFrom(s => "defaultISP"))
-            // .ForMember(d => d.Coordinates, opt => opt.MapFrom(s => "defaultCoordinates"))
-            // .ForMember(d => d.Org, opt => opt.MapFrom(s => "defaultOrg"));
 
-        CreateMap<UpdateUrlDto, URL>(); // Mapping from UpdateUrlDto to URL entity
+        CreateMap<UpdateUrlDto, URL>();
     }
 
 }

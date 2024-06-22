1f5f9cf9c97580a6fc02ec74c2ba76801b41bb8f
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-06-22 00:52:27 -0500

    fixes to 404/405 errors on getURL/updateUrl and prettier formatting changes

--- frontend/next/app/page.tsx
+++ frontend/next/app/page.tsx
@@ -1,16 +1,15 @@
-import CreateUrlForm from "./components/home/createUrl";
+import CreateUrlForm from './components/home/createUrl'
 
 export default function Home() {
-  const IS_DOCKER = process.env.DOCKER_ENV === 'true';
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  console.log('inside home component, printing env vars out..')
-  console.log('API URL IN SERVER COMPONENT: ', process.env.NEXT_PUBLIC_API_URL)
-  console.log('DOCKER ENV VAR IN SERVER COMPONENT: ', process.env.DOCKER_ENV)
+    const IS_DOCKER = process.env.DOCKER_ENV === 'true'
+    const API_URL = process.env.NEXT_PUBLIC_API_URL
+    console.log('inside home component, printing env vars out..')
+    console.log('API URL IN SERVER COMPONENT: ', process.env.NEXT_PUBLIC_API_URL)
+    console.log('DOCKER ENV VAR IN SERVER COMPONENT: ', process.env.DOCKER_ENV)
 
-
-  return (
-    <main className="">
-      <CreateUrlForm />
-    </main>
-  );
+    return (
+        <main className="">
+            <CreateUrlForm />
+        </main>
+    )
 }

9f709aa93b7f3e78619bc084df2dbfa80621b6ee
thitt7 <thitt7@gmail.com>
2024-06-30 19:57:28 -0500

    remove unused packages, tailwind & postCSS config

--- frontend/next/app/lib/getIP.ts
+++ frontend/next/app/lib/getIP.ts
@@ -2,7 +2,7 @@ const getIP = async (): Promise<any> => {
     const response = await fetch('https://api.ipify.org?format=json');
     const data = await response.json();
 
-    console.log('fetched IP: ', data.ip)
+    console.log('fetched IP: ', data.ip);
 
     return data.ip;
 }

b492215e1c92e04f8793ee3047bb9a80ee3faede
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-07-25 21:51:44 -0500

    auth handler wrapper for auth-0

--- frontend/next/app/layout.tsx
+++ frontend/next/app/layout.tsx
@@ -1,26 +1,30 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Header from "@components/layout/header";
-import "./styles/globals.scss";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Header from '@components/layout/header'
+import './styles/globals.scss'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
+import AuthHandler from './components/authHandler'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "URL Tracker",
-  description: "Generate and track your own custom links",
-};
+    title: 'URL Tracker',
+    description: 'Generate and track your own custom links',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <UserProvider>
+                <body className={inter.className}>
+                    <Header />
+                    <AuthHandler>{children}</AuthHandler>
+                </body>
+            </UserProvider>
+        </html>
+    )
 }

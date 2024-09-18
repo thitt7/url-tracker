a61020a7bcfdf182f4f9d0442b2b75724ddc698f
thitt7 <thitt7@gmail.com>
2024-09-17 22:26:36 -0500

    add auth0 pkg, comment out controller w/ build err

--- src/url/Controllers/ApiController.cs
+++ src/url/Controllers/ApiController.cs
@@ -1,23 +1,35 @@
-[Route("api")]
-public class ApiController : Controller
-{
-    [HttpGet("private")]
-    [Authorize]
-    public IActionResult Private()
-    {
-        return Ok(new
-        {
-            Message = "Hello from a private endpoint!"
-        });
-    }
+/* resolve build errors before uncommenting, there is some missing namespace or package reference */
 
-    [HttpGet("private-scoped")]
-    [Authorize("read:messages")]
-    public IActionResult Scoped()
-    {
-        return Ok(new
-        {
-            Message = "Hello from a private-scoped endpoint!"
-        });
-    }
-}
+// using AutoMapper;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using URLService.Data;
+// using URLService.DTOs;
+// using URLService.Entities;
+// using URLService.Services;
+
+// namespace URLService.Controllers;
+
+// [Route("api")]
+// public class ApiController : Controller
+// {
+//     [HttpGet("private")]
+//     [Authorize]
+//     public IActionResult Private()
+//     {
+//         return Ok(new
+//         {
+//             Message = "Hello from a private endpoint!"
+//         });
+//     }
+
+//     [HttpGet("private-scoped")]
+//     [Authorize("read:messages")]
+//     public IActionResult Scoped()
+//     {
+//         return Ok(new
+//         {
+//             Message = "Hello from a private-scoped endpoint!"
+//         });
+//     }
+// }

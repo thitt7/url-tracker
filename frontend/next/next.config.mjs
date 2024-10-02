988d79a738c8bdb815879c3dd2f98fec370a50da
thitt7 <thitt7@gmail.com>
2024-10-02 00:37:06 -0500

    reset next config

--- frontend/next/next.config.mjs
+++ frontend/next/next.config.mjs
@@ -1,38 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  // You can still keep standalone output for serverless deployment if needed
   output: 'standalone',
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.module\.scss$/,
-      use: [
-        isServer ? MiniCssExtractPlugin.loader : 'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true,  // Enable CSS Modules
-            sourceMap: true,
-            importLoaders: 1,
-          },
-        },
-        'sass-loader',
-      ],
-    });
-
-    config.module.rules.push({
-      test: /\.scss$/,
-      exclude: /\.module\.scss$/, // Regular SCSS files
-      use: [
-        isServer ? MiniCssExtractPlugin.loader : 'style-loader',
-        'css-loader',
-        'sass-loader',
-      ],
-    });
-
-    if (isServer) {
-      config.plugins.push(new MiniCssExtractPlugin());
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;

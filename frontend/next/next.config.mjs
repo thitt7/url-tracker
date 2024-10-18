3fe2868075d313817ca1a46ac626b9358ec12c22
thitt7 <thitt7@gmail.com>
2024-10-17 21:47:29 -0500

    debug build process with logs

--- frontend/next/next.config.mjs
+++ frontend/next/next.config.mjs
@@ -1,3 +1,6 @@
+console.log('next config NODE_ENV:', process.env.NODE_ENV);
+console.log('next config NODE_ENV_ARG:', process.env.NODE_ENV_ARG);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 

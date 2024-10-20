1e4a2b70deeb19d094b95868bf6fd9341eeb7cfb
thitt7 <thitt7@gmail.com>
2024-10-20 03:18:04 -0500

    split dockerfiles to dev/prod, add next hot reload

--- frontend/next/next.config.mjs
+++ frontend/next/next.config.mjs
@@ -1,7 +1,6 @@
-console.log('next config NODE_ENV:', process.env.NODE_ENV);
-console.log('next config NODE_ENV_ARG:', process.env.NODE_ENV_ARG);
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone'
+};
 
 export default nextConfig;

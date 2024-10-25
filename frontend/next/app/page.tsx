4aff4c4fecfbf9483ef42523db58633e8ed98656
thitt7 <thitt7@gmail.com>
2024-10-25 01:28:23 -0500

    debug github repo secrets

--- frontend/next/app/page.tsx
+++ frontend/next/app/page.tsx
@@ -2,7 +2,8 @@ import CreateUrlForm from './components/home/createUrl'
 
 export default function Home() {
     // const API_URL = process.env.NEXT_PUBLIC_API_URL;
-    console.log('API URL', `${process.env.NEXT_PUBLIC_API_URL}/api/urls`)
+    console.log('API URL', `${process.env.NEXT_PUBLIC_API_URL}/api/urls`);
+    console.log('DB_HOST', `${process.env.DB_HOST}`);
 
     return (
         <main className="">

89f47412e677d85f89db80f74679e4aed5f49036
Tristan Hitt <39978624+thitt7@users.noreply.github.com>
2026-04-14 00:52:10 -0500

    update next config to use turbopack

--- frontend/next/next.config.mjs
+++ frontend/next/next.config.mjs
@@ -1,41 +1,9 @@
-// import dotenv from 'dotenv';
-
-// dotenv.config({ path: '../../.env' });
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    webpack: (config, { isServer }) => {
-      // Enable tree shaking
-      config.optimization = {
-        ...config.optimization,
-        // usedExports: true,
-      };
-  
-      if (!isServer) {
-        // Client-side only optimizations
-        config.optimization.splitChunks = {
-          chunks: 'all',
-          automaticNameDelimiter: '.',
-          cacheGroups: {
-            defaultVendors: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10,
-              reuseExistingChunk: true,
-            },
-            default: {
-              minChunks: 2,
-              priority: -20,
-              reuseExistingChunk: true,
-            },
-          },
-        };
-  
-        // Ensure production mode is enabled
-        config.mode = 'production';
-      }
-  
-      return config;
-    },
-  };
+  output: 'standalone',
+
+  // Enable Turbopack explicitly (optional but removes warning)
+  turbopack: {},
+};
+
 export default nextConfig;
